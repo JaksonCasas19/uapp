9bbe59ec870acd842c83e8e03e88380d80ebe651
Jakson Casas <jaksoncasas@gmail.com>
2021-01-24 18:14:39 -0500

    first v

---
+++ Assets/UIMandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMandler : MonoBehaviour {
+
+	public void buttonEvent() {
+		
+	}
+}
---
+++ Assets/UIMandler1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMandler1 : MonoBehaviour {
+	public GameObject img;
+
+	public void buttonEvent() {
+		if (img.active) {
+			img.SetActive (false);
+		}else{
+			img.SetActive (true);
+	}
+}
+}
